e7a334260b79d652f1e07defd53f7123393d5f6b
Rajesh01-star <rajeshbehera489@gmail.com>
2026-01-16 14:04:44 +0530

    added import env

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,7 +6,7 @@ const { Pool } = pg
 import * as schema from './schema'
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: import.meta.env.VITE_DATABASE_URL || '',
 })
 
 export const db = drizzle(pool, { schema })
