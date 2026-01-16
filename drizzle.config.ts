98c172a8ad38d06273c0fbe17cfe4f42d4b60f1a
Rajesh01-star <rajeshbehera489@gmail.com>
2026-01-16 14:05:25 +0530

    added process

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || '',
+    url: import.meta.env.VITE_DATABASE_URL || '',
   },
 })
